75c1a1e6155b4ed36bb3b0a9f0b144b730386ad5
ZyabrovaOlga <ingasholga@gmail.com>
2020-11-19 12:32:57 +0700

    Добалена готовая библиотека компонентов Material-UI

--- todo/src/components/InputItem/InputItem.js
+++ todo/src/components/InputItem/InputItem.js
@@ -1,3 +1,14 @@
-const InputItem = () => (<div>InputItem</div>);
+import TextField from '@material-ui/core/TextField';
+
+const InputItem = () => (<div>
+  <TextField
+    label="Текст"
+    id="outlined-margin-dense"
+    defaultValue="Добавить задание"
+    helperText="Введите нужное Вам задание"
+    margin="dense"
+    variant="outlined"
+  />
+</div>);
 
 export default InputItem; 
