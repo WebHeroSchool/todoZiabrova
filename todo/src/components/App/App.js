7def71734fe5c50bb1baed8ad78d27e32e67e019
ZyabrovaOlga <ingasholga@gmail.com>
2020-11-19 12:02:25 +0700

    Подключены стили

--- todo/src/components/App/App.js
+++ todo/src/components/App/App.js
@@ -1,6 +1,7 @@
 import InputItem from '../InputItem/InputItem';
 import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
+import './App.css'
 
 const todoItem = 'Написать новое приложение';
 
@@ -17,8 +18,8 @@ const App = () => {
   }
   ];
   return (
-  <div>
-    <h1>todos</h1>
+  <div className="wrap">
+    <h1 className="wrap__title">Важные дела:</h1>
     <InputItem />
     <ItemList items={items} />
     <Footer count={3} />
