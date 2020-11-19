8cf3597593dd296334bba09141d60b233e033c61
ZyabrovaOlga <ingasholga@gmail.com>
2020-11-19 12:08:44 +0700

    Добавлены модули css

--- todo/src/components/App/App.js
+++ todo/src/components/App/App.js
@@ -1,7 +1,7 @@
 import InputItem from '../InputItem/InputItem';
 import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
-import './App.css'
+import styles from './App.module.css';
 
 const todoItem = 'Написать новое приложение';
 
@@ -18,8 +18,8 @@ const App = () => {
   }
   ];
   return (
-  <div className="wrap">
-    <h1 className="wrap__title">Важные дела:</h1>
+  <div className={styles.wrap}>
+    <h1 className={styles.title}>Важные дела:</h1>
     <InputItem />
     <ItemList items={items} />
     <Footer count={3} />
