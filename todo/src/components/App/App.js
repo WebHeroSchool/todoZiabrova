d31fb17d3a91c7314201a28eb648c85e88b5c963
ZyabrovaOlga <ingasholga@gmail.com>
2020-11-20 14:31:26 +0700

    Проведен рефакторинг

--- todo/src/components/App/App.js
+++ todo/src/components/App/App.js
@@ -1,30 +1,34 @@
+import React from 'react';
 import InputItem from '../InputItem/InputItem';
 import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
 import styles from './App.module.css';
 
-const items = [{
-  id: 1,
-  isDone: true,
-  value: 'Работа'
-},{
-  id: 2,
-  isDone: false,
-  value: 'Приготовление ужина'
-},{
-  id: 3,
-  isDone: true,
-  value: 'Чтение книги'
-}];
+class App extends React.Component {
+  render() {
+    const items = [{
+      id: 1,
+      isDone: true,
+      value: 'Работа'
+    },{
+      id: 2,
+      isDone: false,
+      value: 'Приготовление ужина'
+    },{
+      id: 3,
+      isDone: true,
+      value: 'Чтение книги'
+    }];
+    const itemNumber = items.length;
 
-const itemNumber = items.length;
-
-const App = () => (<div className={styles.wrap}>
-  <h1 className={styles.title}>TODOs</h1>
-  <InputItem />
-  <ItemList items={items}/>
-  <Footer itemNumber={itemNumber}/>
-  </div>
-);
+    return  (<div className={styles.wrap}>
+      <h1 className={styles.title}>TODOs</h1>
+      <InputItem />
+      <ItemList items={items}/>
+      <Footer itemNumber={itemNumber}/>
+      </div>
+    );
+  }
+}
 
 export default App;
