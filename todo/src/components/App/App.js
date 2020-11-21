cbc8a8e9ccaedbdaf0c6f810cebd94c05fe148d9
ZyabrovaOlga <ingasholga@gmail.com>
2020-11-21 14:41:08 +0700

    items перенесен внутрь state

--- todo/src/components/App/App.js
+++ todo/src/components/App/App.js
@@ -5,30 +5,34 @@ import Footer from '../Footer/Footer';
 import styles from './App.module.css';
 
 class App extends React.Component {
-  render() {
-    const items = [{
-      id: 1,
-      isDone: true,
-      value: 'Работа'
-    },{
-      id: 2,
-      isDone: false,
-      value: 'Приготовление ужина'
-    },{
-      id: 3,
-      isDone: true,
-      value: 'Чтение книги'
-    }];
-    const itemNumber = items.length;
+  state = {
+    items: [
+      {
+        id: 1,
+        isDone: true,
+        value: 'Работа'
+      },
+      {
+        id: 2,
+        isDone: false,
+        value: 'Приготовление ужина'
+      },
+      {
+        id: 3,
+        isDone: true,
+        value: 'Чтение книги'
+      }]
+  };
 
+  render() {
     return  (<div className={styles.wrap}>
       <h1 className={styles.title}>TODOs</h1>
       <InputItem />
-      <ItemList items={items}/>
-      <Footer itemNumber={itemNumber}/>
-      </div>
-    );
-  }
-}
+      <ItemList items={this.state.items}/>
+      <Footer itemNumber={this.state.items.length}/>
+      </div>);
+
+    }
+};
 
 export default App;
